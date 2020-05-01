e208316b8c857a2563a0bfb3a6c65fba749a081f
hugogui1998 <L161080071@iztapalapa.tecnm.mx>
2020-05-01 17:19:23 -0500

    Cambios drasticos

--- app/Views/pages/pago_paypal.php
+++ app/Views/pages/pago_paypal.php
@@ -20,9 +20,9 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
-                <li class="nav-item active">
+                <!-- <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
+                </li> -->
                 <li class="nav-item">
                     <a class="nav-link" href="#">Features</a>
                 </li>
